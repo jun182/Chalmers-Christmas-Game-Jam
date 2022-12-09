08f4f7333c135c8abff64db78c410357af1355aa
NightAnimal333 <arturs.umbrasko@protonmail.ch>
2022-12-09 20:59:25 +0100

    Added rigidbody

---
+++ Elf-Employment/scenes/Player.gd
@@ -0,0 +1,17 @@
+extends KinematicBody
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+#var velocity = Vector3.ZERO
+#var gravity = 100
+## Called when the node enters the scene tree for the first time.
+#func _ready():
+#	pass # Replace with function body.
+#
+#func _physics_process(delta):
+#
+#	velocity.y += gravity *delta
+#	velocity = move_and_slide(velocity, Vector3.UP)
